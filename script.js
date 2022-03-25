87a58771858d95de070c723136e0e01f9b12d5e8
Mundane <mundanepeasant@gmail.com>
2022-03-25 14:21:49 -0500

    Etch-a-sketch canvas configured and variable

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 //number of squares to load with
 var squares = 16;
 
+//creating the canvas
+createSquares(squares);
+
+function createSquares(val) {
+    const squareSize = calcSize(val);
+    var parent = document.getElementsByClassName("frame");
+
+    for (var i = 0; i < val; i++) {
+        for (var x = 0; x < val; x++) {
+            const newDiv = document.createElement("div");
+            newDiv.className = "drawing-tile";
+            newDiv.style.height = squareSize + "px";
+            newDiv.style.width = squareSize + "px";
+            parent[0].appendChild(newDiv);
+        }
+    }
+}
+
+function getDimensions(){
+    var div = document.getElementsByClassName("frame");
+    var padding = window.getComputedStyle(div[0]).getPropertyValue('padding');
+    console.log(padding);
+    var width = div[0].offsetWidth - 64;
+    return width;
+}
+
+function calcSize(val) {
+    const size = getDimensions();
+    return size/val;
+}
