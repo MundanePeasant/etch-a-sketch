f81d0d533b321bf0ac6e031d67ad358bae3d3cad
Mundane <mundanepeasant@gmail.com>
2022-03-26 17:56:59 -0500

    Added scroll bar & ability to change canvas size

--- script.js
+++ script.js
@@ -1,57 +1,85 @@
-//number of squares to load with
-var squares = 32;
 
-//creating the canvas
+
+var slider = document.getElementById("slider");
+var squares = slider.value;
+var drawing = false;
+var canvasDrawn = false;
+
 createSquares(squares);
 
+
 function createSquares(val) {
-    const squareSize = calcSize(val);
-    var parent = document.getElementsByClassName("frame");
+  const squareSize = calcSize(val);
+  var parent = document.getElementsByClassName("frame");
 
-    for (var i = 0; i < val; i++) {
-        for (var x = 0; x < val; x++) {
+  for (var i = 0; i < val; i++) {
+    for (var x = 0; x < val; x++) {
+      //creating a new div element
+      const newDiv = document.createElement("div");
 
-            //creating a new div element
-            const newDiv = document.createElement("div");
+      //applying the class name & correct size
+      newDiv.className = "drawing-tile";
+      newDiv.style.height = squareSize + "px";
+      newDiv.style.width = squareSize + "px";
 
-            //applying the class name & correct size
-            newDiv.className = "drawing-tile";
-            newDiv.style.height = squareSize + "px";
-            newDiv.style.width = squareSize + "px";
+      newDiv.addEventListener("mousedown", () => {
+        newDiv.classList.add("drawn");
+        drawing = true;
+      });
 
-
-            newDiv.addEventListener('click', () => {
-                newDiv.classList.add("drawn");
-            });
-
-            newDiv.addEventListener('dragover', () => {
-                newDiv.classList.add("drawn");;
-            });
-
-            //adding the new div to the parent
-            parent[0].appendChild(newDiv);
+      newDiv.addEventListener("mouseover", () => {
+        if (drawing) {
+          newDiv.classList.add("drawn");
         }
+      });
+
+      newDiv.addEventListener("mouseup", () => {
+        if (drawing) {
+          drawing = false;
+        }
+      });
+
+      //adding the new div to the parent
+      parent[0].appendChild(newDiv);
     }
+  }
 }
 
-function getDimensions(){
-    var div = document.getElementsByClassName("frame");
-    var padding = window.getComputedStyle(div[0]).getPropertyValue('padding');
-    var width = div[0].offsetWidth - 64;
-    return width;
+function adjustCanvas(value) {
+  removeSquares();
+  createSquares(value);
+}
+
+function removeSquares() {
+  var parent = document.getElementById("frame");
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function getDimensions() {
+  var div = document.getElementsByClassName("frame");
+  var padding = window.getComputedStyle(div[0]).getPropertyValue("padding");
+  var width = div[0].offsetWidth - 64;
+  return width;
 }
 
 //calculates how big each tile should be
 function calcSize(val) {
-    const size = getDimensions();
-    return size/val;
+  const size = getDimensions();
+  return size / val;
 }
 
 var btn = document.querySelector("button");
-btn.addEventListener('click', () => {
-    const squares = document.getElementsByClassName("drawing-tile");
+btn.addEventListener("click", () => {
+  const squares = document.getElementsByClassName("drawing-tile");
 
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].classList.remove("drawn");
-    }
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].classList.remove("drawn");
+  }
 });
+
+slider.addEventListener("change", () => {
+    removeSquares();
+    createSquares(slider.value);
+  });
