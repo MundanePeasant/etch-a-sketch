72dc16f6679e9855d9d803f53e1a7c399402c1da
Mundane <mundanepeasant@gmail.com>
2022-03-25 20:38:15 -0500

    added drawing and clearing capabilities

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //number of squares to load with
-var squares = 16;
+var squares = 32;
 
 //creating the canvas
 createSquares(squares);
@@ -10,10 +10,25 @@ function createSquares(val) {
 
     for (var i = 0; i < val; i++) {
         for (var x = 0; x < val; x++) {
+
+            //creating a new div element
             const newDiv = document.createElement("div");
+
+            //applying the class name & correct size
             newDiv.className = "drawing-tile";
             newDiv.style.height = squareSize + "px";
             newDiv.style.width = squareSize + "px";
+
+
+            newDiv.addEventListener('click', () => {
+                newDiv.classList.add("drawn");
+            });
+
+            newDiv.addEventListener('dragover', () => {
+                newDiv.classList.add("drawn");;
+            });
+
+            //adding the new div to the parent
             parent[0].appendChild(newDiv);
         }
     }
@@ -22,12 +37,21 @@ function createSquares(val) {
 function getDimensions(){
     var div = document.getElementsByClassName("frame");
     var padding = window.getComputedStyle(div[0]).getPropertyValue('padding');
-    console.log(padding);
     var width = div[0].offsetWidth - 64;
     return width;
 }
 
+//calculates how big each tile should be
 function calcSize(val) {
     const size = getDimensions();
     return size/val;
 }
+
+var btn = document.querySelector("button");
+btn.addEventListener('click', () => {
+    const squares = document.getElementsByClassName("drawing-tile");
+
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].classList.remove("drawn");
+    }
+});
